646fc5e10309a89b002d463be11022d51c935313
707g <daandaan707@gmail.com>
2025-01-16 17:23:49 +0900

    useEffect

--- src/study/App10.jsx
+++ src/study/App10.jsx
@@ -9,16 +9,32 @@ function App10(props) {
     }
 
     const mount = () => {
-        console.log("장착됨2");
+        console.log("장착됨");
         return unmount;
     }
 
-    useEffect(mount, []);
+    useEffect(mount);
+
+    useEffect(() => {
+        console.log(num1);
+        setNum2(num1 + 100);
+    }, [num1]);
+
+    useEffect(() => {
+        console.log(num2);
+    }, [num2]);
+
+    useEffect(() => {
+        console.log(num1)
+        console.log(num2);
+    }, [num1, num2]); // 둘 중 하나라도 상태가 변하면 실행
+
+    useEffect(() => {
+        console.log("마운트!!!")
+    }, []); // 최초에 한 번만 실행
 
     const handleOnClick = () => {
-        const r = num1 + 10;
-        setNum1(r);
-        setNum2(r + 100);
+        setNum1(num1 + 10);
     }
 
     return (
