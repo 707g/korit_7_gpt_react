fdf3f58c10d642acb9ea373953ddd92b1bb19578
707g <daandaan707@gmail.com>
2025-01-20 17:22:05 +0900

    write page css

--- src/pages/IndexPage/style.js
+++
@@ -1,8 +0,0 @@
-import { css } from "@emotion/react"
-
-export const mainLayout = (backgroundColor) => css`
-  margin: 0 auto;
-  width: 1100px;
-  height: 100%;
-  background-color: ${backgroundColor};
-`;
