453b2fd776e9ba4efbb9d88eb8c7263f115b536b
707g <daandaan707@gmail.com>
2025-01-20 17:09:13 +0900

    write pass css

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import IndexPage from "./pages/IndexPage/IndexPage";
 import WritePage from "./pages/WritePage/WritePage";
 import { Global } from "@emotion/react";
 import { global } from "./styles/global";
+import MainLayout from "./components/MainLayout/MainLayout";
 
 function App() {
   return (
     <>
       <Global styles={global} />
 
-      <Routes>
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/write" element={<WritePage />}/>
-      </Routes>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/write" element={<WritePage />}/>
+        </Routes>
+      </MainLayout>
     </>
   );
 }
--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -1,11 +1,11 @@
 /**@jsxImportSource @emotion/react */
-import { mainLayout } from './style';
+import * as s from './style';
 
 import React from 'react';
 
 function IndexPage(props) {
   return (
-    <div css={mainLayout("#fafafa")}>
+    <div>
       
     </div>
   );
